d0d7398a8bf29c7ff8f8e99a93adf79848b47cf7
GodOfGod <1505319276@qq.com>
2024-10-22 10:00:02 +0800

    add flag arg, set --env=prod to use the prod ip

--- main.go
+++ main.go
@@ -3,22 +3,33 @@ package main
 import (
 	api "ThreeCatsGo/api"
 	middleware "ThreeCatsGo/middle_ware"
+	"ThreeCatsGo/tools"
 
 	"github.com/gin-gonic/gin"
 
 	dbCon "ThreeCatsGo/database"
+
+	flag "github.com/spf13/pflag"
 )
 
 func main() {
-	// tools.CreateFolder()
-	// create a router
 	router := gin.Default()
+
 	router.Use(middleware.VerifyToken())
+
 	router.Static("/assets", "save_assets")
-	// pass the router to router handler
+
 	db := dbCon.ConnectDB()
+
 	api.HandleRouter(router, db)
-	// monitor the default port
-	// router.Run("localhost:443")
-	router.RunTLS("172.16.2.91:443", "cert.pem", "cert.key")
+
+	var env *string = flag.String("env", "dev", "current env, dev or prod")
+	flag.Parse()
+	if *env == "dev" {
+		router.Run("localhost:8080")
+	} else if *env == "prod" {
+		router.RunTLS("172.16.2.91:443", "cert.pem", "cert.key")
+	} else {
+		panic(tools.ColoredStr("wrong env arg, you should assign dev or prod to the flag env").Red())
+	}
 }
